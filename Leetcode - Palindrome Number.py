024259fff5d6562166add969457fafc75d6dd361
Muhammad Omer Affan <31851628+momeraffan@users.noreply.github.com>
2022-05-16 15:01:46 +0500

    Create Leetcode - Palindrome Number.py
    First Attempt

---
+++ Leetcode - Palindrome Number.py
@@ -0,0 +1,40 @@
+### Given an integer x, return true if x is palindrome integer.
+
+### An integer is a palindrome when it reads the same backward as forward.
+### For example, 121 is a palindrome while 123 is not.
+ 
+
+### Example 1:
+### Input: x = 121
+### Output: true
+### Explanation: 121 reads as 121 from left to right and from right to left.
+
+### Example 2:
+### Input: x = -121
+### Output: false
+### Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+### Example 3:
+### Input: x = 10
+### Output: false
+### Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+
+### Constraints:
+
+### -231 <= x <= 231 - 1
+
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        y = str(x)
+        yy = list(y)
+        yy.reverse()
+        z = "".join(yy)
+        if y==z:
+            return 1
+        else:
+            return 0
+          
+### Runtime: 101 ms, faster than 32.28% of Python3 online submissions for Palindrome Number.
+### Memory Usage: 13.8 MB, less than 60.63% of Python3 online submissions for Palindrome Number.
