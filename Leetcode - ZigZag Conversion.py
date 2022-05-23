6b6c1ac7a1a90dc9dd55f6e2d136737c880bf8c8
Muhammad Omer Affan <31851628+momeraffan@users.noreply.github.com>
2022-05-23 11:22:56 +0500

    Update Leetcode - ZigZag Conversion.py
    First Attempt

--- Leetcode - ZigZag Conversion.py
+++ Leetcode - ZigZag Conversion.py
@@ -5,19 +5,35 @@ class Solution:
         marker = 0
         for x in range(numRows):
             counter = numRows-x-1
-            if 0 in temp_val1:
-                while True:
-                    try:
-                        while True:
-                            temp_val1.remove(0)
-                    except ValueError:
-                        pass
-            else:
-                for y in range(len(temp_val1)):
-                    if marker < len(s):
+            while 0 in temp_val1:
+                temp_val1.remove(0)
+            #print(temp_val1)
+            #print(temp_val2.replace("0",""))
+            for y in range(len(temp_val1)):
+                if marker < len(temp_val1):
+                    if x == 0:
                         temp_val2+=str(temp_val1[marker])
                         temp_val1[marker] = 0
                         marker+=counter+counter
                     else:
-                        marker = 0
-        print(temp_val2)
+                        temp_val2+=str(temp_val1[marker])
+                        temp_val1[marker] = 0
+                        if marker==0:
+                            marker+=counter+counter
+                            continue
+                        else:
+                            if marker+1 < len(temp_val1):
+                                temp_val2+=str(temp_val1[marker+1])
+                                temp_val1[marker+1] = 0
+                                marker+=counter+counter+1
+                            else:
+                                marker = 0
+                else:
+                    marker = 0
+        for x in temp_val1:
+            if x == 0:
+                continue
+            else:
+                temp_val2+=x
+        temp_val2 = temp_val2.replace("0","")
+        return temp_val2
