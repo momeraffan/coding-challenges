27b06d80214c2ede1608cfa0dcd1660f017be941
Muhammad Omer Affan <31851628+momeraffan@users.noreply.github.com>
2022-05-30 15:44:29 +0500

    Update Leetcode - 3sum.py
    Second Attempt

--- Leetcode - 3sum.py
+++ Leetcode - 3sum.py
@@ -34,11 +34,11 @@ class Solution:
             list_of_lists = []
             temp_nums = nums
             temp_nums.reverse()
-            for y in range(len(nums)-1):
+            for y in range(len(nums)):
                 left_pointer = y
                 right_pointer = len(nums)-1
-
-                while left_pointer<right_pointer-1:
+                
+                while left_pointer<right_pointer:
                     for x in range(left_pointer+1,right_pointer):
                         temp_list = []
                         if nums[left_pointer]+nums[right_pointer]+nums[x] == 0:
@@ -48,10 +48,13 @@ class Solution:
                         else:
                             continue
                         list_of_lists.append(temp_list)
-                    left_pointer+=1
+                    #left_pointer+=1
                     right_pointer-=1
+            
             for x in list_of_lists:
                 x.sort()
             list_of_lists.sort()
             list_of_lists = list(list_of_lists for list_of_lists,_ in groupby(list_of_lists))
             return list_of_lists
+
+# 315 / 318 test cases passed.
